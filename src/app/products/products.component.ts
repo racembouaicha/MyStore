74b6d29ee011dd32cc0af945c0ab7258a4aea63b
Racem1234 <racembouaicha123@gmail.com>
2021-10-31 10:40:07 +0100

    first comment

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AuthService } from '../Services/auth.service';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+    Uid: string
+    successMessage=''
+  constructor( private fs:AngularFirestore,private as:AuthService) {
+    this.as.user.subscribe(user=>{
+      this.Uid=user.uid
+    })
+   }
 
   ngOnInit(): void {
   }
-
+  addProduct(f:any){
+    let data=f.value
+    this.fs.collection("products").doc(this.Uid).set({
+      title:data.title,
+      discription:data.discription,
+      image:data.image,
+      uid:this.Uid,
+    }).then(()=>{
+      this.successMessage="Added"
+    })
+  }
 }
